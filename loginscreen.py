4e0f80bef536efd72b52129d7da5d645799368ec
David Alligood <dralligood@gmail.com>
2018-07-06 23:52:37 -0400

    adding rough login screen

---
+++ loginscreen.py
@@ -0,0 +1,21 @@
+__author__ = 'David'
+
+import kivy
+kivy.require('1.9.0')
+
+from kivy.app import App
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.graphics import Color, Rectangle
+
+# With a box layout we arrange widgets in a horizontal
+# or vertical box
+
+class GridLayoutApp(App):
+
+    def build(self):
+        return GridLayout()
+
+blApp = GridLayoutApp()
+
+blApp.run()
